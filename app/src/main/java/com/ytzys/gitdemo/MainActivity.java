53473d1fc4a28c4ff5c885bc27bec496044e3b33
ytzys <ytzys@mail.ustc.edu.cn>
2019-07-16 14:38:53 +0800

    修改禅道问题

--- app/src/main/java/com/ytzys/gitdemo/MainActivity.java
+++ app/src/main/java/com/ytzys/gitdemo/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // 修改了禅道缺陷
         // 开发了新功能
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
